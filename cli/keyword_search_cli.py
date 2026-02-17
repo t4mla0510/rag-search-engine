da769b18dca3d61bed490b879e44a1f0ed5fb784
t4mla0510 <latritam051004@gmail.com>
2026-02-17 14:39:34 +0700

    ch2:l2 implementing searching with inverted index

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,131 +1,28 @@
-import os
-import json
-import pickle
-import string
 import argparse
-from typing import List, Dict
 
-from nltk.stem import PorterStemmer
-
-def load_stopwords(file_path: str) -> List[str]:
-    """Load stop words from .txt file"""
-    with open(file_path, "r", encoding="utf-8") as file:
-        stop_words = file.read().splitlines()
-    return stop_words
-
-def preprocessing(text: str) -> List[str]:
-    """Lowercase, remove punctuation, tokenizing"""
-    trans_table = str.maketrans('','', string.punctuation)
-    cleaned = text.lower().translate(trans_table)
-    return cleaned.split()
-
-def load_movies(file_path: str) -> List[Dict]:
-    """Load movies from JSON file"""
-    with open(file_path, "r", encoding="utf-8") as file:
-        data = json.load(file)
-    return data.get("movies", [])
-
-def search_movies(query: str, movies: List[Dict], stop_words: List[str], stemmer: PorterStemmer, limit: int = 5) -> List[Dict]:
-    """Return movies where at least one query token matches part of any title token."""
-    query_tokens = preprocessing(query)
-    query_tokens = [stemmer.stem(token) for token in query_tokens if token not in stop_words]
-    results = []
-
-    for movie in movies:
-        title_tokens = preprocessing(movie.get("title", ""))
-        title_tokens = [stemmer.stem(token) for token in title_tokens if token not in stop_words]
-
-        match_found = any(
-            q_token in t_token
-            for q_token in query_tokens
-            for t_token in title_tokens
-        )
-
-        if match_found:
-            results.append(movie)
-            if len(results) == limit:
-                break
-    
-    return results
-
-def print_result(results: List[Dict]):
-    if not results:
-        print("No movies found.")
-        return
-
-    for idx, movie in enumerate(results, start=1):
-        print(f"{idx}. {movie.get('title', '')}")
-
-
-class InvertedIndex:
-    def __init__(self):
-        self.index: Dict[str, set[int]] = {}
-        self.docmap: Dict[int, Dict] = {}
-    
-    def _add_document(self, doc_id: int, text: str) -> None:
-        tokens = text.lower().split()
-        for token in tokens:
-            if token not in self.index:
-                self.index[token] = set()
-            self.index[token].add(doc_id)
-    
-    def get_documents(self, term: str) -> List[int]:
-        term = term.lower()
-        if term in self.index:
-            return sorted(self.index[term])
-        return []
-    
-    def build(self, movies: List[Dict]) -> None:
-        for movie in movies:
-            doc_id = movie["id"]
-            self.docmap[doc_id] = movie
-
-            text = f"{movie["title"]} {movie["description"]}"
-            self._add_document(doc_id, text)
-
-    def save(self) -> None:
-        os.makedirs("cache", exist_ok=True)
-        with open("./cache/index.pkl", "wb") as f:
-            pickle.dump(self.index, f)
-        
-        with open("./cache/docmap.pkl", "wb") as f:
-            pickle.dump(self.docmap, f)
+from lib.keyword_search import build_command, search_command
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
+    subparsers.add_parser("build", help="Build the inverted index")
+
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
-
-    build_parser = subparsers.add_parser("build", help="Build inverted index")
 
     args = parser.parse_args()
 
     match args.command:
-        case "search":
-            print(f"Searching for: {args.query}")
-            
-            # Load movies, stop words and stemmer
-            movies = load_movies("./data/movies.json")
-            stop_words = load_stopwords("./data/stopwords.txt")
-            stemmer = PorterStemmer()
-
-            # Find movies that contain query's keywords
-            results = search_movies(args.query, movies, stop_words, stemmer)
-
-            # Print the result
-            print_result(results)
-            pass
         case "build":
-            index = InvertedIndex()
-            movies = load_movies("./data/movies.json")
-            index.build(movies)
-            index.save()
-
-            docs = index.get_documents("merida")
-            print(f"First document for token 'merida' = {docs[0]}")
-            pass
+            print("Building inverted index...")
+            build_command()
+            print("Inverted index built successfully.")
+        case "search":
+            print("Searching for:", args.query)
+            results = search_command(args.query)
+            for i, res in enumerate(results, 1):
+                print(f"{i}. ({res['id']}) {res['title']}")
         case _:
             parser.print_help()
 
---
+++ cli/lib/keyword_search.py
@@ -0,0 +1,96 @@
+import os
+import pickle
+import string
+from typing import Dict
+from collections import defaultdict, Counter
+
+from nltk.stem import PorterStemmer
+from .search_utils import (
+    CACHE_DIR,
+    DEFAULT_SEARCH_LIMIT,
+    load_movies,
+    load_stopwords
+)
+
+
+class InvertedIndex:
+    def __init__(self) -> None:
+        self.index = defaultdict(set)
+        self.docmap: dict[int, dict] = {}
+        self.index_path = os.path.join(CACHE_DIR, "index.pkl")
+        self.docmap_path = os.path.join(CACHE_DIR, "docmap.pkl")
+    
+    def build(self) -> None:
+        movies = load_movies()
+        for m in movies:
+            doc_id = m["id"]
+            doc_description = f"{m['title']} {m['description']}"
+            self.docmap[doc_id] = m
+            self._add_document(doc_id, doc_description)
+
+    def save(self) -> None:
+        os.makedirs(CACHE_DIR, exist_ok=True)
+        with open(self.index_path, "wb") as f:
+            pickle.dump(self.index, f)
+        with open(self.docmap_path, "wb") as f:
+            pickle.dump(self.docmap, f)
+
+    def load(self) -> None:
+        with open(self.index_path, "rb") as f:
+            self.index = pickle.load(f)
+        with open(self.docmap_path, "rb") as f:
+            self.docmap = pickle.load(f)
+    
+    def get_documents(self, term: str) -> list[int]:
+        doc_ids = self.index.get(term, set())
+        return sorted(list(doc_ids))
+
+    def _add_document(self, doc_id: int, text: str) -> None:
+        tokens = tokenize_text(text)
+        for token in set(tokens):
+            self.index[token].add(doc_id)
+
+
+def build_command():
+    idx = InvertedIndex()
+    idx.build()
+    idx.save()
+
+
+def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
+    idx = InvertedIndex()
+    idx.load()
+    query_tokens = tokenize_text(query)
+    seen, results = set(), []
+    for query_token in query_tokens:
+        matching_doc_ids = idx.get_documents(query_token)
+        for doc_id in matching_doc_ids:
+            if doc_id in seen:
+                continue
+            seen.add(doc_id)
+            doc = idx.docmap[doc_id]
+            results.append(doc)
+            if len(results) >= limit:
+                return results
+    
+    return results
+
+
+def preprocess(text: str) -> str:
+    text = text.lower()
+    text = text.translate(str.maketrans("","", string.punctuation))
+    return text
+
+
+def tokenize_text(text: str) -> list[str]:
+    text = preprocess(text)
+    tokens = text.split()
+    valid_tokens = []
+    for token in tokens:
+        if token:
+            valid_tokens.append(token)
+    stop_words = load_stopwords()
+    filtered_words = [word for word in valid_tokens if word not in stop_words]
+    stemmer = PorterStemmer()
+    stemmed_words = [stemmer.stem(word) for word in filtered_words]
+    return stemmed_words
