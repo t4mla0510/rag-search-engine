04be9efa4c879c021e42ae4be651e8d435b99633
t4mla0510 <latritam051004@gmail.com>
2026-02-17 08:48:30 +0700

    ch1:l7 refactor code and add tokenization

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,7 +1,48 @@
 import argparse
 import json
 import string
+from typing import List, Dict
 
+def preprocessing(text: str) -> List[str]:
+    """Lowercase, remove punctuation, tokenizing"""
+    trans_table = str.maketrans('','', string.punctuation)
+    cleaned = text.lower().translate(trans_table)
+    return cleaned.split()
+
+def load_movies(file_path: str) -> List[Dict]:
+    """Load movies from JSON file"""
+    with open("./data/movies.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+    return data.get("movies", [])
+
+def search_movies(query: str, movies: List[Dict], limit: int = 5) -> List[Dict]:
+    """Return movies where at least one query token matches part of any title token."""
+    query_tokens = preprocessing(query)
+    results = []
+
+    for movie in movies:
+        title_tokens = preprocessing(movie.get("title", ""))
+
+        match_found = any(
+            q_token in t_token
+            for q_token in query_tokens
+            for t_token in title_tokens
+        )
+
+        if match_found:
+            results.append(movie)
+            if len(results) == limit:
+                break
+    
+    return results
+
+def print_result(results: List[Dict]):
+    if not results:
+        print("No movies found.")
+        return
+
+    for idx, movie in enumerate(results, start=1):
+        print(f"{idx}. {movie.get("title", "")}")
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -17,25 +58,13 @@ def main() -> None:
             print(f"Searching for: {args.query}")
             
             # Load movies
-            with open("./data/movies.json", "r", encoding="utf-8") as file:
-                data = json.load(file)
-            movies = data.get("movies", [])
+            movies = load_movies("./data/movies.json")
             
             # Find movies that contain query's keywords
-            results = []
-            trans_table = str.maketrans('', '', string.punctuation)
-
-            query = args.query.lower().translate(trans_table)
-            for movie in movies:
-                title = movie.get("title", "").lower().translate(trans_table)
-                if query in title:
-                    results.append(movie)
-                    if len(results) == 5:
-                        break
+            results = search_movies(args.query, movies)
 
             # Print the result
-            for idx, movie in enumerate(movies, start=1):
-                print(f"{idx}. {movie["title"]}")
+            print_result(results)
             pass
         case _:
             parser.print_help()
