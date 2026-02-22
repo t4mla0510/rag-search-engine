f507669e1e2a7d5286bff4dd3411d45b81b3092d
t4mla0510 <latritam051004@gmail.com>
2026-02-22 18:10:31 +0700

    ch9:l5 add f1 score

--- cli/evaluation_cli.py
+++ cli/evaluation_cli.py
@@ -28,12 +28,14 @@ def main():
         relevant_retrievd_precision = sum(1 for title in resultt_titles if title in golden_titles)
         relevant_retrievd_recall = sum(1 for title in golden_titles if title in resultt_titles)
         
-        precision = relevant_retrievd_precision / limit if limit > 0 else 0.0
+        precision = relevant_retrievd_precision / len(resultt_titles)
         recall = relevant_retrievd_recall / len(golden_titles)
+        f1_score = (2 * precision * recall) / (precision + recall)
         
         print(f"- Query: {query}")
         print(f"  - Precision@{limit}: {precision:.4f}")
         print(f"  - Recall@{limit}: {recall:.4f}")
+        print(f"  - F1 Score: {f1_score:.4f}")
         print(f"  - Retrieved: {",".join(resultt_titles)}")
         print(f"  - Relevant: {",".join(golden_titles)}")
         
