b7151a42e6d41f819373ebf7ea7aaeaef8962e1c
t4mla0510 <latritam051004@gmail.com>
2026-02-23 22:51:16 +0700

    ch12:l4 add image-based search

--- cli/lib/multimodal_search.py
+++ cli/lib/multimodal_search.py
@@ -1,16 +1,59 @@
+import os
 import torch
+import pickle
 from PIL import Image
 from sentence_transformers import SentenceTransformer
 
+from lib.search_utils import load_movies, CACHE_DIR
 
 class MultimodalSearch:
-    def __init__(self, model_name="clip-ViT-B-32"):
+    def __init__(self, model_name="clip-ViT-B-32") -> None:
         self.model = SentenceTransformer(model_name)
+        self.documents: list[dict] = None
+        self.text_embeddings: list[torch.Tensor] = None
+
+    def load_or_create_embeddings(self, documents: list[dict]) -> list[list[float]]:
+        self.documents = documents
+        documents_embeddings_path = os.path.join(CACHE_DIR, "movie_embeddings_tensor.pt")
+        
+        if os.path.exists(documents_embeddings_path):
+            self.text_embeddings = torch.load(documents_embeddings_path)
+            return self.text_embeddings
+        
+        texts = [f"{doc['title']}: {doc['description']}" for doc in documents]
+        embeddings = self.model.encode(texts, convert_to_tensor=True)
+        torch.save(embeddings, documents_embeddings_path)
+        self.text_embeddings = embeddings
+        return self.text_embeddings
     
+    def search_with_image(self, image_path: str) -> list[dict]:
+        image = Image.open(image_path)
+        image_embedding = self.model.encode([image], convert_to_tensor=True)
+
+        results = []
+        for idx, embedding in enumerate(self.text_embeddings):
+            doc = self.documents[idx]
+            score = torch.cosine_similarity(image_embedding, embedding)
+            results.append({
+                "id": doc["id"],
+                "title": doc["title"],
+                "description": doc["description"][:100],
+                "score": float(score)
+            })
+        sorted_results = sorted(results, key= lambda x: x["score"], reverse=True)
+        return sorted_results[:5]
+
     def embed_image(self, image_path: str) -> torch.Tensor:
         image = Image.open(image_path)
         return self.model.encode([image])[0]
-    
+
+
+def image_search_command(image_path: str) -> list[dict]:
+    documents = load_movies()
+    multimodal_search = MultimodalSearch()
+    multimodal_search.load_or_create_embeddings(documents)
+    return multimodal_search.search_with_image(image_path)
+
 
 def verify_image_embedding(image_path: str) -> None:
     multimodal_search = MultimodalSearch()
--- cli/multimodal_search_cli.py
+++ cli/multimodal_search_cli.py
@@ -1,5 +1,5 @@
 import argparse
-from lib.multimodal_search import verify_image_embedding
+from lib.multimodal_search import verify_image_embedding, image_search_command
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Multimodal Search")
@@ -7,11 +7,19 @@ if __name__ == "__main__":
     
     image_embedding_parser = sub_parser.add_parser("verify_image_embedding", help="Get embeddings of an image")
     image_embedding_parser.add_argument("image_path", type=str, help="Image path")
+
+    image_search_parser = sub_parser.add_parser("image_search", help="Search movies with image")
+    image_search_parser.add_argument("image_path", type=str, help="Image path")
     
     args = parser.parse_args()
     
     match args.command:
         case "verify_image_embedding":
             verify_image_embedding(args.image_path)
+        case "image_search":
+            results = image_search_command(args.image_path)
+            for idx, res in enumerate(results, start=1):
+                print(f"{idx}. {res['title']} (similarity: {res['score']:.4f})")
+                print(f"   {res['description']}...")
         case _:
             parser.print_help()
